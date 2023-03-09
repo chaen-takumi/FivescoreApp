7950ea7b1aafcfa109cb38ea8cf49e4a87e12020
chaen-takumi <chaen.ta1227@gmail.com>
2023-03-09 15:58:30 +0900

    6

--- app/src/main/java/com/example/fivescoreapp/GameClear.kt
+++ app/src/main/java/com/example/fivescoreapp/GameClear.kt
@@ -2,10 +2,17 @@ package com.example.fivescoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GameClear : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_clear)
+
+        val btnReplay:Button=findViewById(R.id.btnReplay)
+
+        btnReplay.setOnClickListener {
+            finish()
+        }
     }
 }
